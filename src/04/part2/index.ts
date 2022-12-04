f45866afac246013d1e9c587c8711eeb991777bc
Clare Jolly <clare.jolly@vodafone.com>
2022-12-04 19:17:18 +0000

    day 4 rough part 2 solution

--- src/04/part2/index.ts
+++ src/04/part2/index.ts
@@ -1,3 +1,23 @@
-const part2 = (inputData: string[]): number => {}
+const part2 = (inputData: string[]): number => {
+  const pairs = inputData.map(p =>
+    p.split(',').map(x => {
+      const arr = x.split('-').map(s => Number(s))
+      const test = []
+      let i = arr[0]
+      while (i <= arr[1]) {
+        test.push(i)
+        i++
+      }
+      return test
+    }),
+  )
+
+  const withOverlaps = pairs.filter(p => {
+    const overlapping = p[0].filter(value => p[1].includes(value))
+    return !!overlapping.length
+  })
+
+  return withOverlaps.length
+}
 
 export default part2
