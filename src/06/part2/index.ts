a2816e9845c0fe15efe020469882f671a37931ab
Clare Jolly <clare.jolly@vodafone.com>
2022-12-06 10:27:37 +0000

    day 6 part 2 complete

--- src/06/part2/index.ts
+++ src/06/part2/index.ts
@@ -1,3 +1,12 @@
-const part2 = (inputData: string[]): number => {}
+const part2 = (inputData: string[]): number => {
+  const inputString = inputData[0]
+
+  const data = inputString.split('')
+
+  for (let i = 13; i < data.length; i++) {
+    const checking = data.slice(i - 13, i + 1)
+    if ([...new Set(checking)].length === 14) return i + 1
+  }
+}
 
 export default part2
